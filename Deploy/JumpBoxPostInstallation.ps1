38972508737bce1d417f464d1491b564d966c30a
Tiago Balabuch <Tiago.Balabuch@microsoft.com>
2023-05-24 19:56:12 +0100

    adding more readable error messages

--- Deploy/JumpBoxPostInstallation.ps1
+++ Deploy/JumpBoxPostInstallation.ps1
@@ -1,28 +1,62 @@
 # Install Chocolatey 
 Start-Transcript -Path C:\psLogs.txt -Append
-Set-ExecutionPolicy Unrestricted -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-# Installing Windows Terminal
-Write-Host "Installing Windows Terminal"
-choco install microsoft-windows-terminal -y
 
-# Install dotnet 
-Write-Host "Installing Dotnet 6"
-choco install dotnet-6.0-sdk -y
-Write-Host "Installing Dotnet 7 runtime"
-choco install dotnet-runtime -y
+try {
+    Set-ExecutionPolicy Unrestricted -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))   
+}
+catch {
+    Write-Host "Error to install chocolatey and set execution policy"
+}
 
-# Install Software
-Write-Host "Installing Azure Data Studio"
-choco install azure-data-studio -y
-Write-Host "Installing Azure CLI"
-choco install azure-cli -y
-Write-Host "Installing DotNet Core"
-choco install dotnetcore -y # Check it again if it is still needed
+try {
+    # Installing Windows Terminal
+    Write-Host "Installing Windows Terminal"
+    choco install microsoft-windows-terminal -y
+}
+catch {
+    Write-Host "Error to install Windows Terminal"
+}
 
-Write-Host "Installing PowerShell module"
-Install-PackageProvider -Name NuGet -Force -Confirm:$false
-Install-Module -Name Az.DataMigration -Force -Confirm:$false 
-Write-Host "Az.DataMigration was installed successfully"
+
+try {
+    # Install dotnet 
+    Write-Host "Installing Dotnet 6"
+    choco install dotnet-6.0-sdk -y
+    Write-Host "dotnet-6.0-sdk was installed successfully"
+    Write-Host "Installing Dotnet 7 runtime"
+    choco install dotnet-runtime -y
+    Write-Host "dotnet-runtime was installed successfully"
+    Write-Host "Installing DotNet Core"
+    choco install dotnetcore -y
+    Write-Host "dotnetcore was installed successfully"
+}
+catch {
+    Write-Host "Error to installing dotnet"
+}
+
+    try {
+    # Install Software
+    Write-Host "Installing Azure Data Studio"
+    choco install azure-data-studio -y
+    Write-Host "Azure Data Studio was installed successfully"
+    Write-Host "Installing Azure CLI"
+    choco install azure-cli -y
+    Write-Host "Azure CLI was installed successfully"
+}
+catch {
+    Write-Host "Error to install Azure Data Studio or Azure CLI"
+}
+
+try {
+    Write-Host "Installing PowerShell module"
+    Install-PackageProvider -Name NuGet -Force -Confirm:$false
+    Install-Module -Name Az.DataMigration -Force -Confirm:$false 
+    Write-Host "Az.DataMigration was installed successfully"
+}
+catch {
+    Write-Host "Error to install PowerShell module"
+}
+
 # Function to create folders
 function CreateFolder {
     param (
@@ -49,51 +83,80 @@ $ProjectsTargetDirectory = "C:\temp\projects"
 $SHIRTargetDirectory = "C:\temp\SHIR"
 
 #Create Folders
+
 CreateFolder $OutputTargetDirectory
 CreateFolder $ProjectsTargetDirectory
 CreateFolder $SHIRTargetDirectory
 
-# Installing SqlPackage
-Write-Host "Installing SqlPackage"
-dotnet tool install -g microsoft.sqlpackage
-Write-Host "SqlPackage was installed successfully"
 
-Write-Host "Setting variable"
-$env:Path += "C:\Program Files\dotnet;"
-[Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
-refreshenv
-$env:Path += "C:\Program Files (x86)\Microsoft SDKs\Azure\CLI2\wbin;"
-[Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
-refreshenv
+try {
+    Write-Host "Setting variable"
+    $env:Path += "C:\Program Files\dotnet;"
+    [Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
+    refreshenv
+    $env:Path += "C:\Program Files (x86)\Microsoft SDKs\Azure\CLI2\wbin;"
+    [Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
+    refreshenv      
+}
+catch {
+    
+    Write-Host "Error to set variables: 'C:\Program Files\dotnet;' and 'C:\Program Files (x86)\Microsoft SDKs\Azure\CLI2\wbin;'"
+}
 
-$env:Path += "C:\Windows\System32\config\systemprofile\.dotnet\tools;"
-[Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
-refreshenv
 
-#Printing Env variables
+try {
+    # Installing SqlPackage
+    refreshenv
+    Write-Host "Installing SqlPackage"
+    dotnet tool install -g microsoft.sqlpackage
+    Write-Host "SqlPackage was installed successfully" 
+}
+catch {
+    Write-Host "Error to install SqlPackage" -ErrorAction Stop
+}
+
+try {
+    $env:Path += "C:\Windows\System32\config\systemprofile\.dotnet\tools;"
+    [Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
+    refreshenv
+}
+catch {
+    Write-Host "Error to set variables: 'C:\Windows\System32\config\systemprofile\.dotnet\tools;'" -ErrorAction Stop
+}
+
+refreshenv
+# Printing Env variables
 Write-Host "Printing Env variables"
 Get-ChildItem -Path Env: | format-list
 
-# Installig Azure SQL migration extension for Azure Data Studio
-Write-Host "Installig Azure SQL migration extension"
-Start-Process -filePath "C:\Program Files\Azure Data Studio\bin\azuredatastudio" -ArgumentList "--install-extension Microsoft.sql-migration","--force" -Wait
+# try {
+#     # Downaloading and installig Integration Runtime
+#     Write-Host "Downloading Integration Runtime"
+#     Invoke-WebRequest -Uri https://download.microsoft.com/download/E/4/7/E4771905-1079-445B-8BF9-8A1A075D8A10/IntegrationRuntime_5.29.8528.1.msi -OutFile C:\temp\SHIR\IntegrationRuntime_5.29.8528.1.msi; 
+#     Write-Host "Installing Integration Runtime"
+#     Start-Process msiexec.exe -Wait -ArgumentList '/I C:\temp\SHIR\IntegrationRuntime_5.29.8528.1.msi /quiet'; 
+# }
+# catch {
+#     Write-Host "Error to install Integration Runtime"
+# }
 
-#duplicate and must be deleted
-#Start-Process -filePath "C:\Program Files\Azure Data Studio\bin\azuredatastudio" -ArgumentList @("--install-extension microsoft.sql-migration","--force")
+try {
+    # add extension
+    Write-Host "Adding AZ extension"
+    az extension add --name datamigration --verbose
+}
+catch {
+    Write-Host "Error to add datamigration AZ extension "
+}
 
-# Downaloading and installig Integration Runtime
-Write-Host "Downloading Integration Runtime"
-Invoke-WebRequest -Uri https://download.microsoft.com/download/E/4/7/E4771905-1079-445B-8BF9-8A1A075D8A10/IntegrationRuntime_5.29.8528.1.msi -OutFile C:\temp\SHIR\IntegrationRuntime_5.29.8528.1.msi; 
-Write-Host "Installing Integration Runtime"
-Start-Process msiexec.exe -Wait -ArgumentList '/I C:\temp\SHIR\IntegrationRuntime_5.29.8528.1.msi /quiet';
-
-# add extension
-Write-Host "Adding AZ extension"
-az extension add --name datamigration --verbose
-
-# add extension
-Write-Host "Setting Execution Policy"
-#Set-ExecutionPolicy -Scope LocalMachine -ExecutionPolicy RemoteSigned
+try {
+    # Installig Azure SQL migration extension for Azure Data Studio
+    Write-Host "Installig Azure SQL migration extension"
+    Start-Process -filePath "C:\Program Files\Azure Data Studio\bin\azuredatastudio" -ArgumentList "--install-extension Microsoft.sql-migration","--force"
+}
+catch {
+    Write-Host "Error to install Azure SQL migration extension"
+}
 
 Stop-Transcript
 
